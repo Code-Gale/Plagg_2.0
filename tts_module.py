8556199976c0bf1856c6f61e7d344421055e2741
Code Gale <codedsinger@gmail.com>
2024-02-14 17:58:11 +0100

    tts_module update

--- tts_module.py
+++ tts_module.py
@@ -1,9 +1,10 @@
 import pyttsx3
 
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+engine.setProperty("rate", 200)  # Adjust the rate for faster speech
+
 def speak(text):
     print("Response:", text)
-    # Initialize the text-to-speech engine
-    engine = pyttsx3.init()
-    engine.setProperty("rate", 200)  # Adjust the rate for faster speech
     engine.say(text)
     engine.runAndWait()
