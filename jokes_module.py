560f14516e48d4b9dc039d222eedd52d1371f1c5
Code-Gale <codewhiz001@gmail.com>
2024-02-27 17:16:19 +0100

    jokes_api_url included

--- jokes_module.py
+++ jokes_module.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_joke():
-    joke_api_url = "YOUR_JOKE_API_URL"
+    joke_api_url = "https://v2.jokeapi.dev/joke/Any"
     try:
         response = requests.get(joke_api_url)
         response.raise_for_status()
