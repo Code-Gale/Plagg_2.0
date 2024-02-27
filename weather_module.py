f6c0cde7b10735b0011fb51bdf40c0cbf2e1b653
Code-Gale <codewhiz001@gmail.com>
2024-02-27 13:00:28 +0100

    fetch_weather

--- weather_module.py
+++ weather_module.py
@@ -1,16 +1,39 @@
 import requests
 
-def get_weather(lagos):
-    # Replace 'YOUR_WEATHER_API_KEY' with your actual weather API key
-    weather_api_url = f"YOUR_WEATHER_API_KEY"
-    response = requests.get(weather_api_url)
-    weather_data = response.json()
+def fetch_weather(city):
+    """
+    City to weather
+    :param city: City
+    :return: weather
+    """
+    api_key = 'API KEY HERE'
+    units_format = "&units=metric"
 
-    if weather_data.get("cod") == 200:
-        temperature = weather_data["main"]["temp"]
-        description = weather_data["weather"][0]["description"]
-        weather_response = f"The weather in {lagos} is {description} with a temperature of {temperature}°C."
+    base_url = "http://api.openweathermap.org/data/2.5/weather?q="
+    complete_url = base_url + city + "&appid=" + api_key + units_format
+
+    response = requests.get(complete_url)
+
+    city_weather_data = response.json()
+
+    if city_weather_data["cod"] != "404":
+        main_data = city_weather_data["main"]
+        weather_description_data = city_weather_data["weather"][0]
+        weather_description = weather_description_data["description"]
+        current_temperature = main_data["temp"]
+        current_pressure = main_data["pressure"]
+        current_humidity = main_data["humidity"]
+        wind_data = city_weather_data["wind"]
+        wind_speed = wind_data["speed"]
+
+        final_response = f"""
+        The weather in {city} is currently {weather_description} 
+        with a temperature of {current_temperature} degree celcius, 
+        atmospheric pressure of {current_pressure} hectoPascals, 
+        humidity of {current_humidity} percent 
+        and wind speed reaching {wind_speed} kilometers per hour"""
+
+        return final_response
+
     else:
-        weather_response = f"Sorry, I couldn't fetch weather information for {lagos}."
-
-    return weather_response
+        return "Sorry Sir, I couldn't find the city in my database. Please try again"
