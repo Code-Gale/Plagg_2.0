504280967723808f627c4f19f3d3fb477815d477
Code-Gale <codewhiz001@gmail.com>
2024-02-27 16:47:13 +0100

    speech recognition update

--- speech_recognition_module.py
+++ speech_recognition_module.py
@@ -15,10 +15,3 @@ def get_user_input():
         except sr.RequestError:
             return "There was an issue with the speech recognition service."
 
-def interrupt_activity():
-    stop_listening = False
-    while not stop_listening:
-        user_input = get_user_input()
-        if "enough for now" in user_input:
-            print("Stopping the current activity.")
-            stop_listening = True
